6421725e4b50e57a920c307eb2dc51dedcc11582
Denis Vdovinykh <VerM8n@users.noreply.github.com>
2023-05-07 15:59:38 +0700

    Add alphabet

--- game.cpp
+++ game.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <wchar.h>
 
 using namespace std;
 
 int main(){
     setlocale (LC_ALL,"ru_RU.UTF-8");
+    wchar_t alphabet[33] = {'а','б','в','г','е','ё','ж','з','и','й','к','л','м','н','о','п','р','с','т','у','ф','х','ц','ч','ш','щ','ъ','ы','ь','э','ю','я'};
     
 }
