666e7de972a3e1264e57269da21cb236da0599af
Andreev Mark <124342435+Marknsk@users.noreply.github.com>
2023-05-08 09:40:28 +0700

    Update game.cpp
    + белый фон

--- game.cpp
+++ game.cpp
@@ -9,32 +9,37 @@ Font font;
 
 void ChooseDiff(RenderWindow& window, int x, int y){
     while(1){
-        
+    	
         Vector2i mousePoz = Mouse::getPosition(window);
-
+        
         window.clear();
-
+        
+        RectangleShape Zalivka(Vector2f(x, y));
+    	Zalivka.setPosition(0, 0);
+    	Zalivka.setFillColor(Color::White);
+    	
         RectangleShape Diff1(Vector2f(300, 100));
         Diff1.setPosition(x/2-150, y/3-200);
         Diff1.setFillColor(Color::Blue);
         Text fontDiff1("Easy", font, 35);
         fontDiff1.setPosition(x/2-150+110, y/3-200+25);
         fontDiff1.setFillColor(Color::Black);
-
+        
         RectangleShape Diff2(Vector2f(300, 100));
         Diff2.setPosition(x/2-150, y/3*2-200);
         Diff2.setFillColor(Color::Blue);
         Text fontDiff2("Medium", font, 35);
         fontDiff2.setPosition(x/2-150+85, y/3*2-200+25);
         fontDiff2.setFillColor(Color::Black);
-
+        
         RectangleShape Diff3(Vector2f(300, 100));
         Diff3.setPosition(x/2-150, y-200);
         Diff3.setFillColor(Color::Blue);
         Text fontDiff3("Hard", font, 35);
         fontDiff3.setPosition(x/2-150+110, y-200+25);
         fontDiff3.setFillColor(Color::Black);
-
+        
+        window.draw(Zalivka);
         window.draw(Diff1);
         window.draw(fontDiff1);
         window.draw(Diff2);
@@ -61,14 +66,19 @@ void ChooseDiff(RenderWindow& window, int x, int y){
 
 int main(){
     if(!font.loadFromFile("C:/Windows/Fonts/Arial.ttf")) return EXIT_FAILURE;
-
+    
     setlocale (LC_ALL,"ru_RU.UTF-8");
     
     string alphabet = "?????????????????????????????????";
     
     int x = 1920, y = 1080;
+    // ???????? ????
     RenderWindow window(VideoMode(x, y), "SFML window");
-
+    
+    RectangleShape Zalivka(Vector2f(x, y));
+    Zalivka.setPosition(0, 0);
+    Zalivka.setFillColor(Color::White);
+    
     Texture texture;
     if (!texture.loadFromFile("../1.png")) return EXIT_FAILURE;
     Sprite sprite(texture);
@@ -90,9 +100,8 @@ int main(){
     
     while (window.isOpen())
     {
-
         Vector2i mousePoz = Mouse::getPosition(window);
-
+        
         Event event;
         while (window.pollEvent(event))
         {
@@ -100,6 +109,7 @@ int main(){
                 window.close();
         }
         window.clear();
+        window.draw(Zalivka);
         window.draw(sprite);
         window.draw(But1);
         window.draw(fontBut1);
